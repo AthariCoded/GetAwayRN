7ab29fc0e3e539e951bffdab36e5f909ec39710a
mona187 <monaabusharkh18@gmail.com>
2021-07-29 16:31:32 +0300

    register and login

---
+++ components/Navigation/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "../Home";
+
+const Stack = createStackNavigator();
+export default RootNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#90d4ed",
+        },
+        headerTintColor: "white",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Signin"
+        component={Signin}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Signup"
+        component={Signup}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
---
+++ styles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components/native";
+
+export const HomeBackground = styled.View`
+  flex: 1;
+  align-self: stretch;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+  padding-right: 60px;
+  padding-left: 60px;
+`;
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 50px;
+  font-family: "URW Chancery L, cursive";
+  text-align: center;
+  font-weight: bold;
+  align-items: "center";
+`;
+export const ButtonLogin = styled.View`
+  width: 165px;
+  height: 35px;
+  border-radius: 4px;
+  background: #ffc0cb;
+
+  color: ${(props) => props.theme.black};
+  border: 0px transparent;
+  text-align: center;
+  margin: 5px;
+
+  &:hover {
+    background: #3b5998;
+    opacity: 0.6;
+  }
+`;
+export const ButtonStyling = styled.Text`
+  color: ${(props) => props.theme.backgroundColor};
+  font-size: 22px;
+`;
