e9d2d059600ed4bd09aef2a36b265707a04fda7b
Sarah Almahmid <hpsarontm@gmail.com>
2021-08-01 12:27:45 +0300

    finished add trip

---
+++ components/trips/AddTrip.js
@@ -0,0 +1,101 @@
+import { observer } from "mobx-react";
+import React from "react";
+import { useState } from "react";
+
+//stores
+import authStore from "../../stores/authStore";
+import tripStore from "../../stores/tripStore";
+
+//nav
+import { useNavigation } from "@react-navigation/native";
+
+//components
+import { Alert } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import {
+  AddTripTitle,
+  AddTripLabels,
+  AddTripButton,
+  AddTripButtonText,
+} from "./styles";
+
+const AddTrip = () => {
+  const [trip, setTrip] = useState({
+    title: "",
+    description: "",
+    image: "",
+  });
+
+  const handleAddTrip = async () => {
+    await tripStore.tripAdd(trip, navigation);
+    console.log(trip);
+  };
+
+  const navigation = useNavigation();
+  console.log(authStore.user);
+  if (authStore.user) {
+    // navigation.navigate("AddTrip");
+  } else {
+    Alert.alert(
+      "You must have an account",
+      "You need to sign in to continue.",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        { text: "Sign in", onPress: () => navigation.navigate("Signin") },
+      ],
+      { cancelable: false }
+    );
+  }
+  return (
+    <SafeAreaView>
+      <AddTripTitle> Add New Trip</AddTripTitle>
+      <AddTripLabels>Trip Title</AddTripLabels>
+      <TextInput
+        style={styles.input}
+        onChangeText={(title) => setTrip({ ...trip, title })}
+        placeholder="Trip Title"
+        // keyboardType="numeric"
+      />
+      <AddTripLabels>Trip Description</AddTripLabels>
+      <TextInput
+        style={styles2.input}
+        onChangeText={(description) => setTrip({ ...trip, description })}
+        multiline
+        numberOfLines={8}
+        placeholder="Trip description"
+      />
+      <AddTripLabels>Trip Image Address</AddTripLabels>
+      <TextInput
+        style={styles.input}
+        onChangeText={(image) => setTrip({ ...trip, image })}
+        placeholder="Trip Image Adress"
+      />
+      <AddTripButton onPress={handleAddTrip}>
+        <AddTripButtonText>Add</AddTripButtonText>
+      </AddTripButton>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
+const styles2 = StyleSheet.create({
+  input: {
+    height: 150,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
+export default observer(AddTrip);
--- components/trips/styles.js
+++ components/trips/styles.js
@@ -76,3 +76,32 @@ export const TripListItem = styled.View`
   flex-wrap: wrap;
   margin-bottom: -9px;
 `;
+
+export const AddTripTitle = styled.Text`
+  font-weight: bold;
+  font-size: 30px;
+  margin: 20px;
+  margin-left: 10px;
+`;
+
+export const AddTripLabels = styled.Text`
+  font-weight: 500;
+  font-size: 15px;
+  margin-top: 20px;
+  margin-left: 15px;
+`;
+
+export const AddTripButton = styled.TouchableOpacity`
+  align-self: stretch;
+  align-items: center;
+  padding: 20px;
+  margin: 60px;
+  background-color: ${(props) => props.theme.mainColor};
+  margin-top: 30px;
+`;
+
+export const AddTripButtonText = styled.Text`
+  color: ${(props) => props.theme.backgroundColor};
+  font-weight: bold;
+  font-size: 18px;
+`;
